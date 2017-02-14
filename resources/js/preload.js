ae157453728e2dc9435450e1acd25da8ca3cea05
maptastik <ryancooper729@gmail.com>
2017-02-14 16:29:00 -0500

    Refactored code, updated libraries, fixed breaking changes

--- resources/js/preload.js
+++ resources/js/preload.js
@@ -1,6 +1,6 @@
 // Adapted from code written by Poonam Namdev
 // http://smallenvelop.com/display-loading-icon-page-loads-completely/
-$(window).load(function() {
+$(window).on("load", function() {
 	// Animate loader off screen
-	$(".se-pre-con").fadeOut("slow");;
+	$(".se-pre-con").fadeOut("slow");
 });
